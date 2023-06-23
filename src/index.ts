53c39b2b410a00bf23c94da90edca35a2ad6bbfe
Lion's Habitat <lion@creaselabs.io>
2023-06-23 15:38:04 -0700

    feat: build

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import ConsoleLogMill from '../src/mills/ConsoleLogMill'
 
 export { default as Curator } from './Curator'
 export { default as Workshop } from './Workshop'
-export * from './interfaces'
 export { ConsoleLogMill }
