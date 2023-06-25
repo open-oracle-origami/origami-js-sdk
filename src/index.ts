45406f62b0d73dcad035cc5abd41361fa5fe271f
Gorilla G <gorilla@creaselabs.io>
2023-06-24 18:43:48 -0700

    refactor: force update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ConsoleLogMill from '../src/mills/ConsoleLogMill'
 
 export { default as Curator } from './Curator'
+
 export { default as Workshop } from './Workshop'
 export { ConsoleLogMill }
