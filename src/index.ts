7cf200681c354f3e8e3a2cfac6504266943f1c76
Code Kong <137227957+GorillasHabitat@users.noreply.github.com>
2023-06-24 20:05:17 -0700

    feat(adding base museum and mill to default exports): adding base museum and mill to default exports (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as Curator } from './Curator'
+export { default as BaseMill } from './BaseMill'
+export { default as BaseMuseum } from './BaseMuseum'
+export { default as PubSub } from './services/PubSub'
