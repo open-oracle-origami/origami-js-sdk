e56097ac44f688f375b03ffd5b79f510e1b23b24
Gorilla G <gorilla@creaselabs.io>
2023-06-24 19:55:19 -0700

    feat(adding conditional exports): adding conditional exports
    Adding conditional exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import ConsoleLogMill from '../src/mills/ConsoleLogMill'
-
 export { default as Curator } from './Curator'
-
-export { default as Workshop } from './Workshop'
-export { ConsoleLogMill }
